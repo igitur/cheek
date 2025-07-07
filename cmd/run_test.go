7d2b76dfad275d2a76fca5d586f34a9c91705870
Francois Botha <igitur@gmail.com>
2025-07-07 18:07:54 +0200

    Fix test on Windows

--- cmd/run_test.go
+++ cmd/run_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,5 +11,14 @@ func TestRunCmd(t *testing.T) {
 	// wrong fn will stop the scheduler from going live
 	rootCmd.SetArgs([]string{"run", "../testdata/not-exists.yaml"})
 	err := rootCmd.Execute()
-	assert.Contains(t, err.Error(), "no such file or directory")
+	assert.Error(t, err)
+	expected := getExpectedErrorString()
+	assert.Contains(t, err.Error(), expected)
+}
+
+func getExpectedErrorString() string {
+	if runtime.GOOS == "windows" {
+		return "The system cannot find the file specified."
+	}
+	return "no such file or directory"
 }
