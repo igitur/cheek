09c8f9f37ccad0b51a171c419a9bf7a6ad1418b7
Francois Botha <igitur@gmail.com>
2026-03-12 10:47:01 +0200

    Fix file not found error message for Windows

--- cmd/run_test.go
+++ cmd/run_test.go
@@ -1,14 +1,18 @@
 package cmd
 
 import (
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRunCmd(t *testing.T) {
-	// wrong fn will stop the scheduler from going live
 	rootCmd.SetArgs([]string{"run", "../testdata/not-exists.yaml"})
 	err := rootCmd.Execute()
-	assert.Contains(t, err.Error(), "no such file or directory")
+	if runtime.GOOS == "windows" {
+		assert.Contains(t, err.Error(), "The system cannot find the file specified")
+	} else {
+		assert.Contains(t, err.Error(), "no such file or directory")
+	}
 }
